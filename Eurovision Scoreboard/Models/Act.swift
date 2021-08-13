86ec5a6f9e86a368626fcfcfc8c6b3eae43ccfa3
Daniil Kostitsin <daniil1001k@gmail.com>
2021-08-13 09:40:24 +0200

    Add model documentation

--- Eurovision Scoreboard/Models/Act.swift
+++ Eurovision Scoreboard/Models/Act.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
+/// Model structure representing act information – artist name, song name, and competing country
 struct Act {
+    /// Artist name
     var artistName: String
+    /// Song name
     var songName: String
+    /// Country which sent the act
     var country: Country
 }
 
+// Adopting Codable to be able to save data to disk
 extension Act: Codable { }
 
+// Adopting Hashable for UICollectionViewDiffableDataSource
 extension Act: Hashable { }
